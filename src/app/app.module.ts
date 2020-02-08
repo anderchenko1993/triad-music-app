be1794053d84e98c52ac209c6da18c2733ff3935
“Anderson <anderson_chenko@hotmail.com>
2020-02-08 01:56:07 -0300

    criado components artista, busca, historico e login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuscaComponent } from './busca/busca.component';
+import { ArtistaComponent } from './artista/artista.component';
+import { HistoricoComponent } from './historico/historico.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscaComponent,
+    ArtistaComponent,
+    HistoricoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
